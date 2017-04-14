232ad06d5b7b16317aa5d98eab2b0d2d87904fd2
Eric Salerno <eric.salerno@viacom.com>
2017-04-14 13:43:42 -0400

    Add TDD tests first

--- tests/CamelTest.php
+++ tests/CamelTest.php
@@ -1,8 +1,36 @@
 <?php
-
+/**
+ * Tests for Camel Casing
+ *
+ * @package SalernoLabs
+ * @subpackage Tests
+ * @author Eric Salerno
+ */
 namespace SalernoLabs\Tests\Camelize;
 
 class CamelTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @param $input
+     * @param $expectedOutput
+     * @dataProvider dataProviderForCamelCasing
+     */
+    public function testCamelCasing($input, $expectedOutput)
+    {
+        $camelCaser = new \SalernoLabs\Camelize\Camel();
 
+        $this->assertEquals($expectedOutput, $camelCaser->camelize($input));
+    }
+
+    /**
+     * Data provider for camel casing test
+     */
+    public function dataProviderForCamelCasing()
+    {
+        $output = [
+            ['some_text_WITH_hungarian', 'SomeTextWithHungarian'],
+            ['more random_Strange notation', 'MoreRandomStrangeNotation'],
+            ['  asdf', 'Asdf']
+        ];
+    }
 }
--- tests/UncamelTest.php
+++ tests/UncamelTest.php
@@ -1,8 +1,36 @@
 <?php
-
+/**
+ * Tests for Uncamelize
+ *
+ * @package SalernoLabs
+ * @subpackage Tests
+ * @author Eric Salerno
+ */
 namespace SalernoLabs\Tests\Camelize;
 
 class UncamelTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @param $input
+     * @param $expectedOutput
+     * @dataProvider dataProviderForCamelCasing
+     */
+    public function testUncamelCasing($input, $expectedOutput)
+    {
+        $uncamelCaser = new \SalernoLabs\Camelize\Uncamel();
 
+        $this->assertEquals($expectedOutput, $uncamelCaser->uncamelize($input));
+    }
+
+    /**
+     * Data provider for camel casing test
+     */
+    public function dataProviderForCamelCasing()
+    {
+        $output = [
+            ['SomeTextWithHungarian', 'some_text_with_hungarian'],
+            ['more random_Strange notation', 'more_random_strange_notation'],
+            ['  asdf', 'asdf']
+        ];
+    }
 }
